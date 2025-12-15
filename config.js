5253041081d1a660f40095e692bec54bdfb1184e
geethmapremadasa399-bit <geethmapremadasa399@gmail.com>
2025-12-15 12:52:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'VISPER-MD&29d0ab39-5631-42ea-8afd-9e88089a21d7' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VISPER-MD&d1b65b92-984a-4a4b-aeec-aca0ada54821' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,    
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'vispermd' : process.env.SESSION_NAME, 
 
